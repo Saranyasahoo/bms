234c3d07384341ec46ffdc9808ebc637b6da58dc
Saranyasahoo <saranyasahoo2@gmail.com>
2021-06-01 07:41:23 +0530

    JIRA-124 Creating a Person class

---
+++ GitProject/src/info/inetsolv/Person.java
@@ -0,0 +1,22 @@
+package info.inetsolv;
+
+public class Person {
+
+	private Integer pid;
+	private String name;
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
